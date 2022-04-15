4cf00ca63082aa3d76a50cc07d2f8714e856eec5
Dave Minter <dave@paperstack.com>
2022-04-15 22:33:37 +0200

    Add some resource links to the docs.

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # blog-dns-d - abusing DNS for micro-blogging
 
-The idea is that this will allow micro content (limited to 512 bytes minus the DNS query/answer
+The idea is that this will allow micro content (limited to 255 bytes not counting the DNS query/answer
 overhead) to be rendered when a suitable nerd polls the DNS server
 
 Why? For fun. For small values of fun.
@@ -8,8 +8,8 @@ Why? For fun. For small values of fun.
 ## Status
 
 Lots of hard-coded and duplication, but working for simple
-DNS queries for A and TXT records, and writing useful
-info to stdout as it goes.
+DNS queries for TXT records, currently writing a huge amount
+of garbage to stdout as it goes.
 
 Execute (requires su because it runs on port 53)
 ```bash
@@ -18,19 +18,12 @@ $ sudo ./target/debug/blog-dns-d
 
 Then invoke via dig:
 ```bash
-$ dig +short @127.0.0.1 dnsblog.paperstack.com IN TXT
-127.0.0.1
-```
-or
-```bash
-$ dig +short @127.0.0.1 dnsblog.paperstack.com IN A
-"example=content"
+$ dig +short @127.0.0.1 blog.paperstack.com TXT
+"OUTPUT"
 ```
 
 ## Next steps:
 
-  * Get rid of the duplication
-  * See if I can get sane output with a SINK text record (does dig even support it?)
   * Clean up (e.g. error handling, logging) and parameterise things
   * Work out an approach for content creation and storage!
   * Deploy it somewhere
--- src/main.rs
+++ src/main.rs
@@ -1,194 +1,297 @@
 use std::convert::TryFrom;
+use std::error::Error;
 use std::net::UdpSocket;
 use std::str;
+use zerocopy::byteorder::network_endian::{I32, U16};
+use zerocopy::{AsBytes, FromBytes, FromZeroes};
 
-use bitlab::*;
-use byteorder::{ByteOrder, NetworkEndian};
-
+#[derive(Debug, FromZeroes, FromBytes, AsBytes)]
+#[repr(packed)]
 struct Header {
-    id: u16,
-    qr: bool,
-    opcode: u8,
-    aa: bool,
-    tc: bool,
-    rd: bool,
-    ra: bool,
-    z: bool,
-    ad: bool,
-    cd: bool,
-    rcode: u8,
-    qdcount: u16,
-    ancount: u16,
-    nscount: u16,
-    arcount: u16,
+    id: U16,
+    flags_and_codes: U16,
+    qdcount: U16,
+    ancount: U16,
+    nscount: U16,
+    arcount: U16,
 }
 
-static QUERY: bool = false;
-static INITIAL_OFFSET: u8 = 12;
-static TXT_CLASS:u16 = 16; // (SINK is 40)
-static A_CLASS:u16 = 1; // A
+#[derive(Debug, FromZeroes, FromBytes, AsBytes)]
+#[repr(packed)]
+struct TypeAndClass {
+    query_type: U16,
+    query_class: U16,
+}
+
+#[derive(Debug)]
+struct ResourceRecord {
+    name_offset: U16,
+    record_type: U16,
+    record_class: U16,
+    ttl: I32,
+    record_length: U16,
+    record_data: Vec<u8>,
+}
+
+const QR_MASK: u16 = 0b1000000000000001;
+const OPCODE_MASK: u16 = 0b0111100000000000;
+const AA_MASK: u16 = 0b0000010000000000;
+const TC_MASK: u16 = 0b0000001000000000;
+const RD_MASK: u16 = 0b0000000100000000;
+const RA_MASK: u16 = 0b0000000010000000;
+const Z_MASK: u16 = 0b0000000001110000;
+const RCODE_MASK: u16 = 0b0000000000001111;
+
+trait FlagsAware {
+    fn qr(&self) -> bool;
+    fn opcode(&self) -> u8;
+    fn aa(&self) -> bool;
+    fn tc(&self) -> bool;
+    fn rd(&self) -> bool;
+    fn ra(&self) -> bool;
+    fn z(&self) -> bool;
+    fn rcode(&self) -> u8;
+}
+
+impl FlagsAware for Header {
+    fn qr(&self) -> bool {
+        (self.flags_and_codes.get() & QR_MASK) != 0
+    }
+
+    fn opcode(&self) -> u8 {
+        ((self.flags_and_codes.get() & OPCODE_MASK) >> 1) as u8
+    }
+
+    fn aa(&self) -> bool {
+        (self.flags_and_codes.get() & AA_MASK) != 0
+    }
+
+    fn tc(&self) -> bool {
+        (self.flags_and_codes.get() & TC_MASK) != 0
+    }
+
+    fn rd(&self) -> bool {
+        (self.flags_and_codes.get() & RD_MASK) != 0
+    }
+
+    fn ra(&self) -> bool {
+        (self.flags_and_codes.get() & RA_MASK) != 0
+    }
+
+    fn z(&self) -> bool {
+        (self.flags_and_codes.get() & Z_MASK) != 0
+    }
+
+    fn rcode(&self) -> u8 {
+        ((self.flags_and_codes.get() & RCODE_MASK) >> 12) as u8
+    }
+}
+
+
+const QUERY: bool = false;
+const INITIAL_OFFSET: u8 = 12;
+const MAX_UDP_QUERY_SIZE: usize = 512;
 
 fn main() -> std::io::Result<()> {
-    {
-        let socket = UdpSocket::bind("127.0.0.1:53")?;
+    print!("Binding socket...");
+    let socket = UdpSocket::bind("127.0.0.1:53")?;
+    println!("bound.");
 
-        loop {
-            let mut buf = [0; 512];
-            let (_amt, src) = socket.recv_from(&mut buf)?;
-            println!("Buffer: {:?}", &buf);
+    let mut query_buffer = [0; MAX_UDP_QUERY_SIZE];
+    let mut response_buffer = [0; MAX_UDP_QUERY_SIZE];
 
-            let header = extract_header(&mut buf);
-            if &header.qr == &QUERY {
-                // TODO: Could iterate over (and enforce) qdcount times!
+    loop {
+        println!("Listening...");
+        let (_amt, src) = socket.recv_from(&mut query_buffer)?;
 
-                let (qname, mut next_offset) = read_qname(INITIAL_OFFSET, &mut buf);
-                let qtype = NetworkEndian::read_u16(&buf[next_offset..(next_offset+2)]);
-                next_offset+=2;
-                let qclass = NetworkEndian::read_u16(&buf[next_offset..(next_offset+2)]);
-                next_offset+=2;
+        let header = Header::ref_from(&query_buffer[0..12]).unwrap();
+        println!("ID: {}, (Flags), QD: {}, AN: {}, NS: {}, AR: {}", header.id, header.qdcount, header.ancount, header.nscount, header.arcount);
+        println!("QR: {}, OPCODE: {}, AA: {}, TC: {}, RD: {}, RA: {}, Z: {}, RCODE: {}", header.qr(), header.opcode(), header.aa(), header.tc(), header.rd(), header.ra(), header.z(), header.rcode());
 
-                // TODO: Log these and only in debug level(s)
-                println!("Query Name: {}", &qname);
-                println!("id: {}, qr: {}, opcode: {}, aa: {}, tc: {}, rd: {}, ra: {}, z: {}, ad: {}, cd: {}, rcode: {}, qdcount: {}, ancount: {}, nscount: {}, arcount: {}",
-                         &header.id, &header.qr, &header.opcode, &header.aa, &header.tc, &header.rd, &header.ra, &header.z, &header.ad, &header.cd, &header.rcode, &header.qdcount, &header.ancount, &header.nscount, &header.arcount);
-                println!("qtype: {}", &qtype); // 1 is A, 5 is CNAME, 2 is Name Servers, F is mail servers, 40 is SINK etc.!
-                println!("qclass: {}", &qclass); // Always 1 for IN(ternet)
+        // TODO: Handle bad record counts
 
-                // TODO: Build and return a meaningful response!
-                buf[2] |= 0b1000_0000; // Set response bit
-                buf[3] |= 0b1000_0000; // Set recursion available bit
-
-                if qtype == TXT_CLASS {
-                    println!("TXT class record requested!");
-                    // Set the right header values
-                    NetworkEndian::write_u16( &mut buf[6 .. 8], 1); // 1 answer
-                    NetworkEndian::write_u16( &mut buf[10 .. 12], 0); // 0 additional records
-
-                    // Then we repeat a bunch of stuff (more or less)!
-
-                    // qname - point to the received one
-                    NetworkEndian::write_u16(&mut buf[next_offset .. (next_offset+2)], 12); // Offset 12 (max is 2^14 as 2 bits used to indicate pointer)
-                    buf[next_offset] = 0b1100_0000; // Pointer type
-                    next_offset += 2;
-
-                    // type
-                    NetworkEndian::write_u16(&mut buf[next_offset .. (next_offset+2)], qtype);
-                    next_offset += 2;
-
-                    // class
-                    NetworkEndian::write_u16(&mut buf[next_offset .. (next_offset+2)], qclass);
-                    next_offset += 2;
-
-
-                    NetworkEndian::write_u32( &mut buf[next_offset .. (next_offset+4)], 0); // TTL
-                    next_offset += 4;
-
-                    let text = b"example=content";
-                    let rdlength:u16 = (text.len() + 1) as u16;
-                    NetworkEndian::write_u16(&mut buf[next_offset .. (next_offset+2)], rdlength); // RDLENGTH (4 octets)
-                    next_offset += 2;
-
-                    buf[next_offset] = text.len() as u8;
-                    next_offset += 1;
-
-                    buf[next_offset .. next_offset + usize::try_from(text.len()).unwrap()].clone_from_slice(text);
-                    next_offset += usize::try_from(rdlength).unwrap();
-
-                    let output = &buf[0 .. next_offset];
-                    println!("Output: {:?}", output);
-                    socket.send_to(output, &src)?;
-                } else if qtype == A_CLASS {
-                    println!("A class record requested!");
-
-                    // Set the right header values
-                    NetworkEndian::write_u16( &mut buf[6 .. 8], 1); // 1 answer
-                    NetworkEndian::write_u16( &mut buf[10 .. 12], 0); // 0 additional records
-
-                    // Then we repeat a bunch of stuff (more or less)!
-
-                    // qname - point to the received one
-                    NetworkEndian::write_u16(&mut buf[next_offset .. (next_offset+2)], 12); // Offset 12 (max is 2^14 as 2 bits used to indicate pointer)
-                    buf[next_offset] = 0b1100_0000; // Pointer type
-                    next_offset += 2;
-
-                    // type
-                    NetworkEndian::write_u16(&mut buf[next_offset .. (next_offset+2)], qtype);
-                    next_offset += 2;
-
-                    // class
-                    NetworkEndian::write_u16(&mut buf[next_offset .. (next_offset+2)], qclass);
-                    next_offset += 2;
-
-                    // Now the actual answer stuff...
-                    NetworkEndian::write_u32( &mut buf[next_offset .. (next_offset+4)], 0); // TTL
-                    next_offset += 4;
-                    NetworkEndian::write_u16(&mut buf[next_offset .. (next_offset+2)], 4); // RDLENGTH (4 octets)
-                    next_offset += 2;
-
-                    // DATA...
-                    buf[next_offset] = 127;
-                    next_offset += 1;
-                    buf[next_offset] = 0;
-                    next_offset += 1;
-                    buf[next_offset] = 0;
-                    next_offset += 1;
-                    buf[next_offset] = 1;
-                    next_offset += 1;
-
-                    let output = &buf[0 .. next_offset];
-                    println!("Output: {:?}", output);
-                    socket.send_to(output, &src)?;
-                } else {
-                    socket.send_to(&buf, &src)?;
+        if header.qr() == QUERY {
+            let (qname, next_offset, raw_query_name) = match read_qname(INITIAL_OFFSET, &query_buffer) {
+                Ok((qname, next_offset)) => {
+                    // Returning...
+                    // qname as a nice normal string
+                    // the offset of the next octet in the input buffer
+                    // the raw slice representing the qname so we can steal it for building the response buffer
+                    (qname, next_offset, &query_buffer[INITIAL_OFFSET as usize..next_offset])
                 }
+                Err(err) => {
+                    println!("ERROR: {:?}", err);
+                    // Something went horribly wrong; not even trying for a response here...
+                    continue;
+                }
+            };
+            println!("Query name: {}", qname);
 
-            } else {
-                println!("Incoming packet wasn't a query! id: {}", &header.id);
+            let type_and_class = TypeAndClass::ref_from(&query_buffer[next_offset..next_offset + 4]).unwrap();
+            println!("Query Type: {}, Query Class: {}", query_type_to_string_slice(type_and_class.query_type.get()), query_class_to_string_slice(type_and_class.query_class.get()));
+
+            if !qname.eq_ignore_ascii_case("blog.paperstack.com") || type_and_class.query_type.get() != TYPE_TXT {
+                println!("Not a suitable qname query, or not expecting TXT type");
+                // TODO: Fail more politely!
+                respond_with_error();
+                continue;
             }
+
+            println!("Creating header response");
+            let mut response_flags_and_codes: u16 = header.flags_and_codes.get() & 0b0_1111_0_0_1_0_000_0000; // Copy OPCODE from input
+            response_flags_and_codes |= 0b1000000000000000; // Set QR to say this is a query response
+
+            let response_header = Header {
+                id: header.id,
+                flags_and_codes: U16::from(response_flags_and_codes),
+                qdcount: U16::from(1), // 1 question record
+                ancount: U16::from(1), // 1 answer records
+                nscount: U16::from(0), // No authority records
+                arcount: U16::from(0), // No additional records
+            };
+
+            // Clear out any previous responses
+            _ = &response_buffer.fill(0);
+
+            let mut index = 0;
+            append_to_buffer(&mut response_buffer, &response_header.as_bytes(), &mut index);
+
+            println!("Creating qname response");
+
+            let raw_qname_index = index;
+            let raw_qname_as_bytes: &[u8] = &raw_query_name.as_bytes();
+            append_to_buffer(&mut response_buffer, raw_qname_as_bytes, &mut index);
+            append_to_buffer(&mut response_buffer, &type_and_class.as_bytes(), &mut index);
+
+            println!("Creating resource record response");
+
+            let response_text = "OUTPUT".as_bytes();
+            let response_text_length: u8 = response_text.len() as u8;
+            let mut response_data = Vec::new();
+            response_data.push(response_text_length);
+            response_data.extend_from_slice(response_text);
+
+            let answer_record = ResourceRecord {
+                name_offset: U16::from(0b1100_0000_0000_0000 as u16 | raw_qname_index as u16),
+                record_type: U16::from(TYPE_TXT),
+                record_class: U16::from(QCLASS_IN),
+                ttl: I32::from(86400i32),
+                record_length: U16::from(response_data.len() as u16),
+                record_data: response_data.into(),
+            };
+
+            append_to_buffer(&mut response_buffer, &answer_record.name_offset.as_bytes(), &mut index);
+            append_to_buffer(&mut response_buffer, &answer_record.record_type.as_bytes(), &mut index);
+            append_to_buffer(&mut response_buffer, &answer_record.record_class.as_bytes(), &mut index);
+            append_to_buffer(&mut response_buffer, &answer_record.ttl.as_bytes(), &mut index);
+            append_to_buffer(&mut response_buffer, &answer_record.record_length.as_bytes(), &mut index);
+            append_to_buffer(&mut response_buffer, &answer_record.record_data.as_slice(), &mut index);
+
+            print!("Sending response...");
+
+            socket.send_to(&response_buffer[0..index], &src)?;
+
+            println!("sent.");
+        } else {
+            respond_with_error();
         }
     }
 }
 
-fn read_qname(initial_offset: u8, buf: &mut [u8; 512]) -> (String, usize) {
+// TODO: Make this actually return a proper error response
+fn respond_with_error() {
+    // Build a header with the error code set
+    // Build the qname (if one was provided)
+    // Send the response
+    println!("ERROR");
+}
+
+fn append_to_buffer(response_buffer: &mut [u8; 512], value: &[u8], index: &mut usize) {
+    _ = &response_buffer[*index..*index + value.len()].copy_from_slice(value);
+    *index += value.len();
+}
+
+
+fn read_qname(initial_offset: u8, buf: &[u8; 512]) -> Result<(String, usize), Box<dyn Error>> {
     let mut qname = Vec::new();
     let mut offset: u8 = initial_offset;
-    let mut lsize = buf[usize::try_from(offset).unwrap()];
+    let mut lsize = buf[usize::try_from(offset)?];
     while lsize > 0 {
         offset += 1;
-        let range_begin = usize::try_from(offset).unwrap();
-        let range_end: usize = usize::try_from(lsize + offset).unwrap();
-        let label = str::from_utf8(&buf[range_begin..range_end]).unwrap();
+        let range_begin = usize::try_from(offset)?;
+        let range_end: usize = usize::try_from(lsize + offset)?;
+        let label = str::from_utf8(&buf[range_begin..range_end])?;
         qname.push(label);
 
         offset += lsize;
-        lsize = buf[usize::try_from(offset).unwrap()];
+        lsize = buf[usize::try_from(offset)?];
     }
 
-    ( qname.join("."), usize::try_from(offset+1).unwrap() )
+    // Per the RFC no alignment needed here:
+    // > The domain name terminates with the zero length octet for the null label of the root.  Note
+    // > that this field may be an odd number of octets; no padding is used.
+
+    Ok((qname.join("."), usize::try_from(offset + 1)?))
 }
 
-// TODO: Is there a better way to do the unwrap stuff here?
-fn extract_header(buf: &mut [u8; 512]) -> Header {
-    let flags: u16 = NetworkEndian::read_u16(&buf[2..4]);
-    let header = Header {
-        id: NetworkEndian::read_u16(&buf[0..2]),
-        qr: flags.get_bit(0).unwrap(),
-        opcode: flags.get_u8(1, 3).unwrap(),
-        aa: flags.get_bit(5).unwrap(),
+const TYPE_A: u16 = 1;        // 1 a host address
+const TYPE_NS: u16 = 2;       // 2 an authoritative name server
+const TYPE_MD: u16 = 3;       // 3 a mail destination (Obsolete - use MX)
+const TYPE_MF: u16 = 4;       // 4 a mail forwarder (Obsolete - use MX)
+const TYPE_CNAME: u16 = 5;    // 5 the canonical name for an alias
+const TYPE_SOA: u16 = 6;      // 6 marks the start of a zone of authority
+const TYPE_MB: u16 = 7;       // 7 a mailbox domain name (EXPERIMENTAL)
+const TYPE_MG: u16 = 8;       // 8 a mail group member (EXPERIMENTAL)
+const TYPE_MR: u16 = 9;       // 9 a mail rename domain name (EXPERIMENTAL)
+const TYPE_NULL: u16 = 10;    // 10 a null RR (EXPERIMENTAL)
+const TYPE_WKS: u16 = 11;     // 11 a well known service description
+const TYPE_PTR: u16 = 12;     // 12 a domain name pointer
+const TYPE_HINFO: u16 = 13;   // 13 host information
+const TYPE_MINFO: u16 = 14;   // 14 mailbox or mail list information
+const TYPE_MX: u16 = 15;      // 15 mail exchange
+const TYPE_TXT: u16 = 16;     // 16 text strings
+const QTYPE_AXFR: u16 = 252;  // 252 A request for a transfer of an entire zone
+const QTYPE_MAILB: u16 = 253; // 253 A request for mailbox-related records (MB, MG or MR)
+const QTYPE_MAILA: u16 = 254; // 254 A request for mail agent RRs (Obsolete - see MX)
 
-        tc: flags.get_bit(6).unwrap(),
-        rd: flags.get_bit(7).unwrap(),
-        ra: flags.get_bit(8).unwrap(),
-        z: flags.get_bit(9).unwrap(),
-        ad: flags.get_bit(10).unwrap(),
-        cd: flags.get_bit(11).unwrap(),
+fn query_type_to_string_slice(p: u16) -> &'static str {
+    match p {
+        TYPE_A => "A",
+        TYPE_NS => "NS",
+        TYPE_MD => "MD",
+        TYPE_MF => "MF",
+        TYPE_CNAME => "CNAME",
+        TYPE_SOA => "SOA",
+        TYPE_MB => "MB",
+        TYPE_MG => "MG",
+        TYPE_MR => "MR",
+        TYPE_NULL => "NULL",
+        TYPE_WKS => "WKS",
+        TYPE_PTR => "PTR",
+        TYPE_HINFO => "HINFO",
+        TYPE_MINFO => "MINFO",
+        TYPE_MX => "MX",
+        TYPE_TXT => "TXT",
+        QTYPE_AXFR => "AXFR",
+        QTYPE_MAILB => "MAILB",
+        QTYPE_MAILA => "MAILA",
+        _ => "UNKNOWN"
+    }
+}
 
-        rcode: flags.get_u8(12, 4).unwrap(),
+const QCLASS_IN: u16 = 1; // IN - INternet
+const QCLASS_CS: u16 = 2; // CS - CSNET, obsolete
+const QCLASS_CH: u16 = 3; // CH - CHaosNet, obsolete
+const QCLASS_HS: u16 = 4; // HS - HeSiod, obsolte
+const QCLASS_ANY: u16 = 255; // * - ANY class
 
-        qdcount: NetworkEndian::read_u16(&buf[4..6]),
-        ancount: NetworkEndian::read_u16(&buf[6..8]),
-        nscount: NetworkEndian::read_u16(&buf[8..10]),
-        arcount: NetworkEndian::read_u16(&buf[10..12]),
-    };
-    header
+fn query_class_to_string_slice(p: u16) -> &'static str {
+    match p {
+        QCLASS_IN => "IN",
+        QCLASS_CS => "CS",
+        QCLASS_CH => "CH",
+        QCLASS_HS => "HS",
+        QCLASS_ANY => "*",
+        _ => "UNKNOWN"
+    }
 }
